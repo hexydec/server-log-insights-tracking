81b399093154cff32636f69a77c7369d4716717e
Hexydec <will@hexydec.com>
2024-10-24 07:41:27 +0100

    Updated tracking code to use a `fetch()` request to beacon the analytics to the server. Added a duration metric to track how long the page has been in view. Updated the tracking endpoint to be a JSON file as it is small and benign. Updated `README.md` with description and usage examples.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# server-log-insights-tracking
-Tracking code to improve tracking with Server Log Insights
+# Server Log Insights Tracking Code
+
+This repository provides tracking code for [Server Log Insights](https://serverloginsights.com), a server logs analytics tool.
+
+## Usage
+
+Add this repository as a dependency in your `package.json` file using NPM:
+
+```
+npm i --save hexydec/server-log-insights-tracking#main
+```
+
+You can build the tracking into your javascript bundle like this:
+
+```javascript
+import tracking from "../node_modules/server-log-insights-tracking/src/tracking.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+	tracking();
+});
+```
+
+You can also update the base address of the tracking endpoint like this:
+
+```javascript
+tracking("/the-base-address-of-my-website/");
+```
+
+The tracking code uses the endpoint [`/slid.json`](src/slid.json). The endpoint doesn't have to exist for the system to pick up the data in your server logs, but it is better to place the file in your root directory so that your logs are not filled with 404 errors.
+
+## Licence
+
+The MIT License (MIT). Please see [License File](LICENCE) for more information.
--- src/tracking.js
+++ src/tracking.js
@@ -1,13 +1,18 @@
-export default () => {
+export default base => {
 
 	// setup variables
 	let key = "slid",
-		url = "/" + key + ".png?",
+		url = (base || "/") + key + ".json?",
 		win = window,
 		doc = document,
 		robot = win.callPhantom || win._phantom || win.phantom || win.__nightmare || navigator.webdriver || doc.__selenium_unwrapped || doc.__webdriver_evaluate || doc.__driver_evaluate,
 		params = {e: "pageview", u: localStorage.getItem(key), w: screen.width, h: screen.height, l: navigator.language, v: robot ? "robot" : "human"},
-		geturl = params => url + (new URLSearchParams(params)).toString();
+		send = params => fetch(url + (new URLSearchParams(params)).toString(), {
+			method: "HEAD",
+			credentials: "omit",
+			keepalive: true
+		})
+		visible = null;
 
 	// generate random identifier
 	if (params.u === null) {
@@ -17,33 +22,31 @@ export default () => {
 
 	// make request so we can pick it up in the server logs
 	if (new URL(document.referrer)?.hostname !== location.hostname) {
-		fetch(geturl(params), {
-			method: "HEAD",
-			credentials: "omit"
-		});
+		send(params);
 	}
 
-	// send beacon when they leave the page
-	if ("sendBeacon" in navigator) {
-
-		// retrieve load times
-		const observer = new PerformanceObserver((entryList) => {
-			const entry = entryList.getEntries()[0];
-			params.i = entry.domInteractive; // initial load
-			params.t = entry.domComplete; // total load
-		});
-		observer.observe({type: "navigation", buffered: true});
-	  
-		// send beacon when the user navigates away
-		win.addEventListener("visibilitychange", () => {
-			if (doc.visibilityState === "hidden") {
-				params.e = "navigate";
-				const link = doc.querySelector("a[href]:focus");
-				if (link !== null) {
-					params.n = link.href;
-				}
-				navigator.sendBeacon(geturl(params));
+	// retrieve load times
+	const observer = new PerformanceObserver((entryList) => {
+		const entry = entryList.getEntries()[0];
+		params.i = entry.domInteractive; // initial load
+		params.t = entry.domComplete; // total load
+	});
+	observer.observe({type: "navigation", buffered: true});
+	
+	// send beacon when the user navigates away
+	win.addEventListener("visibilitychange", () => {
+		if (doc.visibilityState === "hidden") {
+			params.e = "unload";
+			const link = doc.querySelector("a[href]:focus");
+			if (link !== null) {
+				params.n = link.href;
 			}
-		});
-	}
+			if (visible !== null) {
+				params.d = Math.floor((Date.now() - visible) / 1000);
+			}
+			send(params);
+		} else if (doc.visibilityState === "visible") {
+			visible = Date.now();
+		}
+	});
 };
