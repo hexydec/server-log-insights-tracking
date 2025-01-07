45436754d7ec315417c6211181445889c6a9f511
Hexydec <will@hexydec.com>
2025-01-07 18:38:11 +0000

    Added a test page for robots and reworked how they are tested for. Updated dependencies.

--- src/robots.js
+++ src/robots.js
@@ -3,23 +3,39 @@ import props from "./worker.js";
 export default () => {
 	return new Promise(resolve => {
 		let win = window,
-			doc = document
+			doc = document,
 			nav = navigator, 
-			hint = nav.userAgentData.getHighEntropyValues(["fullVersionList"]),
-			robot = win.callPhantom || win._phantom || win.phantom || win.__nightmare || nav.webdriver || doc.__selenium_unwrapped || doc.__webdriver_evaluate || doc.__driver_evaluate || win.chrome || win.external?.toString().contains("Sequentum") || hint["brands"].some(obj => obj.brand.contains("HeadlessChrome")),
-			code = 'const obj={};try{const obj={u:navigator.userAgent,l:JSON.stringify(navigator.languages),h:navigator.hardwareConcurrency,c:false,v:null,r:null },e=new Error(),canvas=new OffscreenCanvas(1,1),webgl=canvas.getContext("webgl"),ext=webgl.getExtension("WEBGL_debug_renderer_info"),props={v:ext.UNMASKED_VENDOR_WEBGL,r:ext.UNMASKED_RENDERER_WEBGL};try{Object.defineProperty(e,"stack",{get(){obj.c=true}});console.log(e)}catch(stack){}try{for(let key in props){obj[key]=webgl.getParameter(props[key])}}catch(webgl){}}catch(err){}self.postMessage(obj);',
+			hint = nav.userAgentData?.getHighEntropyValues(["fullVersionList"]) || {},
+			robot = (win.callPhantom || win._phantom || win.phantom || win.__nightmare || nav.webdriver || doc.__selenium_unwrapped || doc.__webdriver_evaluate || doc.__driver_evaluate || win.external?.toString().includes("Sequentum") || hint.brands?.some(obj => obj.brand.includes("HeadlessChrome"))) !== undefined,
+			e = new Error(),
+			code = 'const o={u:navigator.userAgent,l:JSON.stringify(navigator.languages),h:navigator.hardwareConcurrency,c:false,v:null,r:null};try{w=(new OffscreenCanvas(1,1)).getContext("webgl"),e=w.getExtension("WEBGL_debug_renderer_info"),p={v:e.UNMASKED_VENDOR_WEBGL,r:e.UNMASKED_RENDERER_WEBGL};for(let k in p){o[k]=w.getParameter(p[k])}}catch(e){}self.postMessage(o)',
 			blob = new Blob([code], {type: "application/javascript"}),
-			url = URL.createObjectURL(blob),
-			worker = new Worker(url);
-		worker.onmessage = e => {
-			const obj = props();
-			for (let key in obj) {
-				if (obj[key] !== e.data[key]) {
+			url = URL.createObjectURL(blob);
+
+		// check if the console exists
+		if (!robot) {
+			Object.defineProperty(e, "stack", {
+				get() {
 					robot = true;
-					break;
 				}
-			}
+			});
+			console.log(e);
+		}
+
+		// wait for message
+		if (!robot) {
+			(new Worker(url)).onmessage = e => {
+				const obj = props();
+				for (let key in obj) {
+					if (obj[key] !== e.data[key]) {
+						robot = true;
+						break;
+					}
+				}
+				resolve(robot);
+			};
+		} else {
 			resolve(robot);
-		};
+		}
 	});
 }
